b0e0c38c0f1debc3a138b17f0550789f0bedb25c
DiegoVitorSantosCosta <diegovitorsantoscosta2017@gmail.com>
2025-12-18 22:04:11 -0300

    primeiro commit

---
+++ console/lib/console.dart
@@ -0,0 +1,38 @@
+
+void main() {
+
+  print('Hello Dart 👋');
+
+  var idade = 28;
+  double altura = 1.78;
+  bool programador = true;
+
+  print('Idade: $idade');
+  print('Altura: $altura');
+  print('Programador: $programador');
+  final p = Pessoa('Bad Bryan', 29);
+
+  p.apresentar(Pessoa('testando parametro', 234234));
+
+  int somar(int salario, int conta) {
+    return salario - conta;
+  }
+
+  print(somar(10000, 20));
+}
+
+class Pessoa {
+  String nome;
+  int idade;
+
+  Pessoa(this.nome, this.idade);
+
+  void apresentar(Pessoa obj) {
+    print('parametros : ' + obj.nome);
+    print('Meu nome é $nome e tenho $idade');
+  }
+
+  bool ehImpar() {
+    return 3.isOdd;
+  }
+}
---
+++ console/lib/final.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  /**
+   * O Final é uma constante mas em runtime
+   * Significa que podemos atribuir ele classes
+   * ou coisas constantes como numero.
+   * Diferente do const que não aceita classes
+   * Pois a classe não é run time ela é compilada.
+   * 
+   */
+  final String nome = 'Bad Bryan';
+}
